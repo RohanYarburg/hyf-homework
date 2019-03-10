7503bbeaa01bd77348c3e1ab2d8b35f75d264536
Rohan Yarburg <rohan.yarburg@gmail.com>
2019-03-10 14:55:06 +0100

    added first file 'age-ify'

---
+++ javascript/javascript1/week1/age-ify.js
@@ -0,0 +1,7 @@
+var yearOfBirth;
+var yearFuture;
+yearOfBirth=1998;
+yearFuture=2025;
+var age=yearFuture - yearOfBirth;
+var outcome="You will be "+age+" years old in "+yearFuture;
+console.log (outcome);
